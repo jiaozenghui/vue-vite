62e5848252ff256561ac839162f86b8e53e46002
jiaozh1 <jiaozh1@lenovo.com>
2022-10-02 22:22:30 +0800

    blog style change

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -9,8 +9,6 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
     ACol: typeof import('ant-design-vue/es')['Col']
-    ACollapse: typeof import('ant-design-vue/es')['Collapse']
-    ACollapsePanel: typeof import('ant-design-vue/es')['CollapsePanel']
     ALayout: typeof import('ant-design-vue/es')['Layout']
     ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
     ARow: typeof import('ant-design-vue/es')['Row']
@@ -19,6 +17,7 @@ declare module '@vue/runtime-core' {
     Layouts: typeof import('./src/components/layouts/Layouts.vue')['default']
     LButton: typeof import('./src/components/basics/lbuttons/LButton.vue')['default']
     LeftSide: typeof import('./src/components/LeftSide.vue')['default']
+    LenovoButton: typeof import('./src/components/buttons/LenovoButton.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
---
+++ frontend/src/components/buttons/LenovoButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+    confs: {}
+})
+</script>
+
+<template>
+<a-button class="tets" type="primary">Primary Button</a-button>
+</template>
+
+<style scoped>
+</style>
